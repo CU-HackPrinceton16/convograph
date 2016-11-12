ee56f6a91d2b73a2100cfe5c0858c939503b2afa
Jake Lee <louisoel@gmail.com>
2016-11-12 02:26:24 -0500

    added MSFT speech recognition python SDK

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import os
 import requests
+import msft_sr
+import 
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
@@ -9,9 +11,12 @@ def main():
 	localdir = os.path.dirname(__file__)
 	sample_filename = os.path.join(localdir, "samples/sample1.wav")
 	print(sample_filename)
-	sectionsoutput = os.path.join(localdir, "sections/section{0}.wav")
+	sections_filename = os.path.join(localdir, "sections/section{0}.wav")
 	sound_input = AudioSegment.from_wav(sample_filename)
 
+	sound_input.set_channels(1)				#Mono required
+	sound_input.set_frame_rate(16000)		#16k required
+	sound_input.set_sample_width(2)			#16bit required
 	splitaudio = split_on_silence(sound_input,
 		min_silence_len = 500,
 		silence_thresh = -40,
@@ -20,8 +25,14 @@ def main():
 
 	section_count = 0
 	for i, section in enumerate(splitaudio):
-		section.export(sectionsoutput.format(i), format="wav")
+		section.export(sections_filename.format(i), format="wav")
 		section_count = i
 
 	print(section_count)
+
+	#scale this later
+
+	section_file = AudioSegment.from_wav(sections_filename.format(1))
+
+
 main()

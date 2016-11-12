ef83a53ac4c54947d1c24e7c3a60f55bfc302ff9
Jake Lee <louisoel@gmail.com>
2016-11-12 11:27:29 -0500

    branch initial commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pydub.silence import split_on_silence
 
 def main():
 	localdir = os.path.dirname(__file__)
-	sample_filename = os.path.join(localdir, "samples/sample2.wav")
+	sample_filename = os.path.join(localdir, "samples/sample1.wav")
 	print(sample_filename)
 	sections_filename = os.path.join(localdir, "sections/section{0}.wav")
 	sound_input = AudioSegment.from_wav(sample_filename)
@@ -17,7 +17,7 @@ def main():
 	sound_input.set_frame_rate(16000)		#16k required
 	sound_input.set_sample_width(2)			#16bit required
 	splitaudio = split_on_silence(sound_input,
-		min_silence_len = 300,
+		min_silence_len = 400,
 		silence_thresh = -40,
 		keep_silence = 200
 	)
@@ -26,6 +26,7 @@ def main():
 	for i, section in enumerate(splitaudio):
 		section.export(sections_filename.format(i), format="wav", bitrate="16k")
 		section_count = i
+
 
 	print(str(section_count) + " sections identified")
 
